e75328701562c0010784fb2ef8b067ac074e55b1
brook <brook.zhang@aonhewitt.com>
2018-07-26 14:18:54 +0800

    change root directory to webapp

--- config/src/main/java/com/yianyouxuan/config/ConfigApplication.java
+++ config/src/main/java/com/yianyouxuan/config/ConfigApplication.java
@@ -3,9 +3,8 @@ package com.yianyouxuan.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-//@EnableDiscoveryClient
+
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigApplication {
