9c28c2f2ef3cd55f61f51c115956ec8fd63ac371
brook <brook.zhang@aonhewitt.com>
2018-07-26 13:52:28 +0800

    remove eureka client in config

--- config/src/main/java/com/yianyouxuan/config/ConfigApplication.java
+++ config/src/main/java/com/yianyouxuan/config/ConfigApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigApplication {
