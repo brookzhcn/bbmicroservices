6f0f7488d3f9188e1970d992d90d0a9dedcc1f04
brook <brook.zhang@aonhewitt.com>
2018-08-01 18:21:19 +0800

    修改配置文件

--- message/src/main/java/com/yianyouxuan/message/MessageApplication.java
+++ message/src/main/java/com/yianyouxuan/message/MessageApplication.java
@@ -3,6 +3,7 @@ package com.yianyouxuan.message;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
@@ -12,6 +13,7 @@ import java.util.concurrent.Executor;
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableAsync
+@EnableFeignClients
 public class MessageApplication{
 
 	public static void main(String[] args) {
